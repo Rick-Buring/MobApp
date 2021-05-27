fc7b166fa3bdc1d71e366dfbffc0491e522eff7d
R <r.buring@student.avans.nl>
2021-05-27 17:49:52 +0200

    Created SplashScreenActivity

---
+++ app/src/main/java/com/example/mobapp/SplashScreenActivity.java
@@ -0,0 +1,19 @@
+package com.example.mobapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> startActivity(new Intent(this, MainActivity.class)), 3000);
+
+    }
+}
