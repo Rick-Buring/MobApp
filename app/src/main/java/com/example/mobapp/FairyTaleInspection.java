72a827c912a8dce2afdc6a22499c26ef3d88df16
R <r.buring@student.avans.nl>
2021-06-01 15:03:26 +0200

    testActivity

--- app/src/main/java/com/example/mobapp/FairyTaleInspection.java
+++ app/src/main/java/com/example/mobapp/FairyTaleInspection.java
@@ -1,10 +1,10 @@
 package com.example.mobapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class fairy_tale_inspection extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FairyTaleInspection extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
