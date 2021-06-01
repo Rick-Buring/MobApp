7c06c279c3024f1f004d88677f1f0dbe363b5c94
R <r.buring@student.avans.nl>
2021-06-01 15:16:39 +0200

    Created binding for topbar

--- app/src/main/java/com/example/mobapp/FairyTaleInspection.java
+++ app/src/main/java/com/example/mobapp/FairyTaleInspection.java
@@ -3,12 +3,19 @@ package com.example.mobapp;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.example.mobapp.databinding.ActivityFairyTaleInspectionBinding;
+
 
 public class FairyTaleInspection extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fairy_tale_inspection);
+        ActivityFairyTaleInspectionBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_fairy_tale_inspection);
+        binding.setActivity(this);
+
+
     }
 }
