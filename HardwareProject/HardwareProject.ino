a8bb8f0bae53bacc224976f868684cda5da742f4
Jesse <jm.krijgsman@Student.avans.nl>
2021-06-01 14:57:14 +0200

    Added MQTT to hardware

--- HardwareProject/HardwareProject.ino
+++ HardwareProject/HardwareProject.ino
@@ -5,8 +5,13 @@
 #include <LiquidCrystal_I2C.h>
 #include <ESP32Servo.h>
 
+// pinnumber for house servo's
 const int SERVO_HOUSE_1_PIN = 2;
+const float BLOW_TOTAL_COMPLETE = 200;
+const char *MQTT_TOPIC_BLOWER = "ti/1.4/b1/blower/speed";
+const char *MQTT_TOPIC_TOTALBLOW = "ti/1.4/b1/blower/total";
 
+// pinnumber for blowreader
 const int BLOW_READER_PIN = 34;
 
 // Setting the pins for the LED's
@@ -14,13 +19,49 @@ const int NR_OF_LEDS = 3;
 const int LED_PINS[NR_OF_LEDS] = {12, 13, 14};
 const int LED_CHANNELS[NR_OF_LEDS] = {1, 2, 3}; // Voor de ESP32 LED control module
 
-Servo servoHouse1;
-int pos = 0;
 
+// END OF CONSTANT VALUES   //
+// ------------------------ //
+// MQTT SETUP SETTINGS      //
+
+
+// Zelf instellen voor je eigen WLAN
+const char *WLAN_SSID = "Ziggo89DC852";
+const char *WLAN_ACCESS_KEY = "zhbNc5f3fjst";
+
+// CLIENT_ID moet uniek zijn, dus zelf aanpassen (willekeurige letters en cijfers)
+const char *MQTT_CLIENT_ID = "MQTTExampleTryout_zeer_unieke_code_PPOPKKKINBR4352Ad";
+
+// Gegevens van de MQTT broker die we in TI-1.4 kunnen gebruiken
+const char *MQTT_BROKER_URL = "sendlab.nl";
+const int MQTT_PORT = 11884;
+const char *MQTT_USERNAME = "ti";
+const char *MQTT_PASSWORD = "tiavans";
+
+// Definieer de te gebruiken Quality of Service (QoS)
+const int MQTT_QOS = 0;
+
+WiFiClient wifiClient;
+//WiFiClientSecure wifiClient; // Om een met TLS beveiligde verbinding te kunnen gebruiken
+PubSubClient mqttClient(wifiClient);
+
+
+// END OF MQTT SETTINGS     //
+// ------------------------ //
+// DECALRATION OF VARIABLES //
+
+
+// Servo controls
+Servo servoHouse1;
+
+// Led controls
 bool ledIsOn = true; // Voor de ingebouwde LED, soort keep-alive knipperlicht
 byte ledIntensities[4] = {0, 0, 0, 0};
 
+// blower controls
 int currentBlow = 0;
+int totalBlowPercent = 0;
+
 
 /*
  * Stel de LED helderheid in
@@ -31,6 +72,20 @@ void setLedIntensity(int ledNr, byte intensity)
 {
   byte pwmValue = map(intensity, 255, 0, 0, 255);
   ledcWrite(LED_CHANNELS[ledNr], pwmValue);
+}
+
+/*
+ * De MQTT client callback die wordt aangeroepen bij elk bericht dat
+ * we van de MQTT broker ontvangen
+ */
+void mqttCallback(char *topic, byte *payload, unsigned int length)
+{
+  // Logging
+  Serial.print("MQTT callback called for topic ");
+  Serial.println(topic);
+  Serial.print("Payload length ");
+  Serial.println(length);
+
 }
 
 void setup()
@@ -55,12 +110,47 @@ void setup()
 
   // Open de verbinding naar de seriële terminal
   Serial.begin(115200);
-  Serial.println("ESP32 MQTT example");
+  Serial.println("ESP32 MOBAPP B!");
+
+   // Zet de WiFi verbinding op
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  Serial.println("Connecting to ");
+  Serial.println(WLAN_SSID);
+  WiFi.begin(WLAN_SSID, WLAN_ACCESS_KEY);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Om een met TLS beveiligde verbinding te kunnen gebruiken zonder certificaten
+  //wifiClient.setInsecure(); // Skip de controle, niet ideaal maar werkt voorlopig wel
+
+  // Zet de MQTT client op
+  mqttClient.setServer(MQTT_BROKER_URL, MQTT_PORT);
+  mqttClient.setCallback(mqttCallback);
+
+  // Maak verbinding met de MQTT broker
+  if (!mqttClient.connect(MQTT_CLIENT_ID, MQTT_USERNAME, MQTT_PASSWORD))
+  {
+    Serial.println("Failed to connect to MQTT broker");
+  }
+  else
+  {
+    Serial.println("Connected to MQTT broker");
+  }
 }
 
 
 void loop()
 {
+  // Nodig om de MQTT client zijn werk te laten doen
+  mqttClient.loop();
+  
   if (!servoHouse1.attached()) {
     servoHouse1.setPeriodHertz(50); // standard 50 hz servo
     servoHouse1.attach(33, 500, 2400); // Attach the servo after it has been detatched
@@ -69,34 +159,35 @@ void loop()
 
   // Reading the blow-motor, if value changed print it 
   int newBlow = analogRead(BLOW_READER_PIN) / 20;
+
+  // upload turning speed
   if(newBlow != currentBlow) {
+    char payload[30];
+    sprintf(payload, "%d", newBlow);
+    mqttClient.publish(MQTT_TOPIC_BLOWER, payload);
+  
     Serial.print("blowing changed: ");
-    Serial.println(newBlow);
+    Serial.println(payload);
   }
+
+  // updating percentage blow
+  int percent = ((newBlow / BLOW_TOTAL_COMPLETE) * 100);
+  if (percent != 0) {
+    totalBlowPercent += percent;
+    char payloadPer[10];
+    sprintf(payloadPer, "%d", totalBlowPercent);
+    mqttClient.publish(MQTT_TOPIC_TOTALBLOW, payloadPer);
+
+    Serial.print("blowing percentage changed: ");
+    Serial.println(payloadPer);
+    Serial.println(totalBlowPercent);  
+  }
+  
   currentBlow = newBlow;
 
   // Laat de ingebouwde LED knipperen
   ledIsOn = !ledIsOn;
   digitalWrite(LED_BUILTIN, ledIsOn);
-
-  if(ledIsOn == true) {
-    ledIntensities[0] = 255;
-    ledIntensities[1] = 0;
-    ledIntensities[2] = 255;
-
-    servoHouse1.write(0);
-  } else {
-    ledIntensities[0] = 0;
-    ledIntensities[1] = 255;
-    ledIntensities[2] = 0;
-
-    servoHouse1.write(180);
-  }
-  setLedIntensity(0, ledIntensities[0]);
-  setLedIntensity(1, ledIntensities[1]);
-  setLedIntensity(2, ledIntensities[2]);
-
   
-  
-  delay(5000);
+  delay(500);
 }
