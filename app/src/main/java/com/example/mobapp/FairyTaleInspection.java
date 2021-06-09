99e6980a0ba66e0b91aa9f8747adf60f96d7d5da
Stijn van Tilburg <stijnvtilburg@hotmail.com>
2021-06-09 15:05:26 +0200

    Added animation + text layout improvement

--- app/src/main/java/com/example/mobapp/FairyTaleInspection.java
+++ app/src/main/java/com/example/mobapp/FairyTaleInspection.java
@@ -3,6 +3,7 @@ package com.example.mobapp;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.TextSwitcher;
 import android.widget.ViewFlipper;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -34,8 +35,9 @@ public class FairyTaleInspection extends AppCompatActivity implements ShowPopup.
         binding.setActivity(this);
         binding.setData(fairytale);
 
-
         this.viewFlipper = findViewById(R.id.flipper);
+        viewFlipper.setInAnimation(this, R.anim.slide_in_right);
+        viewFlipper.setOutAnimation(this, R.anim.slide_out_left);
         fairytale.views.forEach(fairytaleStep -> {
             View inflatedView = LayoutInflater.from(this).inflate(fairytaleStep.getView(), null);
 
