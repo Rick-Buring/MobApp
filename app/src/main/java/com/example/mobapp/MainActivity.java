dab01eae60fdb78ff73e8711226b5937f595c6bb
R <r.buring@student.avans.nl>
2021-06-09 15:46:24 +0200

    removed ping

--- app/src/main/java/com/example/mobapp/MainActivity.java
+++ app/src/main/java/com/example/mobapp/MainActivity.java
@@ -27,8 +27,6 @@ public class MainActivity extends AppCompatActivity implements FairyTaleAdapter.
         for (Fairytale tale: Fairytale.fairytales) {
             manager.subscribeToTopic(topicLocation + tale.getTopic());
         }
-
-        manager.publishMessage(topicLocation + "availability/request", " ");
     }
 
     @Override
