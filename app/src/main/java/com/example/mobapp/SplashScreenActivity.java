95e224ef148b1371414885667652983311ccd859
Jesse <jm.krijgsman@student.avans.nl>
2021-06-10 15:39:14 +0200

    comments for popup

--- app/src/main/java/com/example/mobapp/SplashScreenActivity.java
+++ app/src/main/java/com/example/mobapp/SplashScreenActivity.java
@@ -7,14 +7,20 @@ import android.os.Looper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+/**
+ * Creates the opening screen, with the logo in the centre
+ */
 public class SplashScreenActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+        // Opening a new MQTTManager, it wil Establish a new connection
         new MQTTManager(this);
 
+        // Start the animation on the screen
         new Handler(Looper.getMainLooper()).postDelayed(()-> {
             Intent intent = new Intent(getApplicationContext(), MainActivity.class);
             startActivity(intent);
