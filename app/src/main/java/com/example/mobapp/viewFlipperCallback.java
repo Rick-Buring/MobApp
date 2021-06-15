09b676958d22c3aff2522b32e64cdf45bcbd475c
Jesse <jm.krijgsman@Student.avans.nl>
2021-06-15 13:58:01 +0200

    ViewFlippelCallback comments

--- app/src/main/java/com/example/mobapp/viewFlipperCallback.java
+++ app/src/main/java/com/example/mobapp/viewFlipperCallback.java
@@ -1,8 +1,22 @@
 package com.example.mobapp;
 
+/**
+ * Callback interface to change the active image on the viewFlipper
+ */
 public interface viewFlipperCallback {
 
+    /**
+     * Makes the ViewFlipper go to the next image
+     */
     void flipperNext();
+
+    /**
+     * Makes the ViewFlipper go to the previous image
+     */
     void flipperPrevious();
+
+    /**
+     * Makes the ViewFlipper go to the next image, skipping one
+     */
     void flipperSkipOne();
 }
